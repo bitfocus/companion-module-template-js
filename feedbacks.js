95da11f88ca491aa1ccba4a08f2b483bfdef5749
Quinn <Quinn.Matthews@lrs.com>
2023-01-10 20:18:33 -0600

    Add template for feedbacks and variables

---
+++ feedbacks.js
@@ -0,0 +1,33 @@
+const { combineRgb } = require('@companion-module/base')
+
+module.exports = async function (self) {
+	self.setFeedbackDefinitions({
+		ChannelState: {
+			name: 'Example Feedback',
+			type: 'boolean',
+			label: 'Channel State',
+			defaultStyle: {
+				bgcolor: combineRgb(255, 0, 0),
+				color: combineRgb(0, 0, 0),
+			},
+			options: [
+				{
+					id: 'num',
+					type: 'number',
+					label: 'Test',
+					default: 5,
+					min: 0,
+					max: 10,
+				},
+			],
+			callback: (feedback) => {
+				console.log('Hello world!', feedback.options.num)
+				if(feedback.options.num > 5) {
+					return true
+				} else {
+					return false
+				}
+			},
+		},
+	})
+}
